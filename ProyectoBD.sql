e8d3802c12bf5fd72d9960de2554f63a2e7c7af5
alanfon078 <ls23120218@surguanajuato.tecnm.mx>
2025-10-12 21:41:09 -0600

    Add files via upload

--- ProyectoBD.sql
+++ ProyectoBD.sql
@@ -6,12 +6,12 @@ create table user(
     apellidos varchar(100) not null,
     user varchar(20) not null unique,
     password varchar (256) not null,
-    status enum('active','inactive') not null,
+    status boolean not null default true,
     fechaCreacion timestamp default current_timestamp,
     correo varchar(50) not null unique,
     telefono varchar(10) not null unique,
     fechaNacimiento date not null
     );
-insert into user(nombre, apellidos, user, password, status, correo, telefono, fechaNacimiento) 
-values('Alan Yael', 'Fonseca Ruiz', 'Admin', sha2('Blackops078.', 256), 1, 'yalan078@gmail.com','4451011393', '2005-12-30');
+insert into user(nombre, apellidos, user, password, correo, telefono, fechaNacimiento) 
+values('Alan Yael', 'Fonseca Ruiz', 'Admin', sha2('Blackops078.', 256), 'yalan078@gmail.com','4451011393', '2005-12-30');
 select * from user;
